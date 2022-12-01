1c972922c2e0dfe3d28412f47ea214326d362ce9
ZikGrafsky <silentzzephyr@gmail.com>
2022-12-01 11:45:41 +0300

    upd dcarrotate

--- dcarotate.c
+++ dcarotate.c
@@ -11,13 +11,13 @@ char	**dcarotate(char **dca)
 
     if (!dca || !dca[0])
         return (0);
-    tmp = (char **) malloc(sizeof (char *) * (ft_strlen(dca[0]) + 1));
+    tmp = (char **) malloc(sizeof (char *) * (strlen(dca[0]) + 1));
     if (!tmp)
         return (0);
-    tmp[ft_strlen(dca[0])] = NULL;
+    tmp[strlen(dca[0])] = NULL;
     i = 0;
-    while (i < ft_strlen(dca[0]))
-        tmp[i++] = ft_calloc(sizeof (char), dcalen(dca) + 1);
+    while (i < strlen(dca[0]))
+        tmp[i++] = calloc(sizeof (char), dcalen(dca) + 1);
     i = -1;
     while (dca[++i])
     {
