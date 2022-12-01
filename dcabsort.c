202bf87371f901287d4437130e97dd4a4ac51d40
ZikGrafsky <silentzzephyr@gmail.com>
2022-12-01 10:56:02 +0300

    upd bsort to use std stdcmp

--- dcabsort.c
+++ dcabsort.c
@@ -6,7 +6,7 @@
 char **dcabsort(char **arr){//bubble - sort method
     for(int i = 0; i < dcalen(arr); ++i)
         for (int j = 0; j < dcalen(arr) - i - 1; ++j)
-            if(ft_strcmp_nc(arr[j], arr[j+1]) > 0){
+            if(strcmp(arr[j], arr[j+1]) > 0){
                 char *str = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = str;
